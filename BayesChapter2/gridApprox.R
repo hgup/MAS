957470822119411900defc0d14d99241cc6a4679
hgup <hurshgupta1@gmail.com>
2023-03-25 06:29:50 +0000

    extra notes on grid approx

--- BayesChapter2/gridApprox.R
+++ BayesChapter2/gridApprox.R
@@ -1,5 +1,7 @@
 n <- 30
 
+# we call it a grid because it represents the combination of
+# all possible parameter values
 p_grid <- seq( from=0 , to=1 , length.out=n )
 
 # use any one of the priors
@@ -7,8 +9,13 @@ p_grid <- seq( from=0 , to=1 , length.out=n )
 prior <- ifelse( p_grid < 0.5 , 0 , 1 )
 prior <- exp( -5*abs( p_grid - 0.5 ) )
 
+# Grid approx procedure
+
+
+# compute the likelihood of every p value, Pr(p|data)
 likelihood <- dbinom( 6 , size=9 , prob=p_grid )
 
+# find the posterior. Pr(data|p) * 
 unstd.posterior <- likelihood * prior
 
 posterior <- unstd.posterior/sum(unstd.posterior)
